3b5e3029465ee330d9e67f867a529458d89a5488
Aniket Dashpute <aniketsd96@gmail.com>
2021-01-28 17:44:57 +0530

    Adding library files and basic VS/FS code in html

--- ParticleSystemMain.js
+++ ParticleSystemMain.js
@@ -9,9 +9,9 @@ function main()
     const gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
 
     // Only continue if WebGL is available and working
-    if (gl == null)
+    if (null == gl)
     {
-        console.log("Failed to get rendering context for WebGL");
+        console.log("main() Failed to get rendering context for WebGL");
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
@@ -20,6 +20,18 @@ function main()
     gl.clearColor(0.6, 0.0, 0.6, 1.0);
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // get the vertex and fragment shader strings
+    var vsSource = document.getElementById("vertex-shader").textContent;
+
+    var fsSource = document.getElementById("fragment-shader").textContent;
+
+    // Initialize shaders
+    if (!initShaders(gl, vsSource, fsSource))
+    {
+		console.log('main() Failed to intialize shaders.');
+		return;
+	}
 }
 
 window.onload = main();
